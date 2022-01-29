da7a0ac65033839f5ab7c7aa604389b3a00fa0ff
BastiennM <bastienmetais79@gmail.com>
2022-01-29 20:58:55 +0100

    add dynamic buider to display categorie

--- lib/models/homeCategorieService.dart
+++ lib/models/homeCategorieService.dart
@@ -15,23 +15,31 @@ class CategorieService {
   CategorieService({
     required this.id,
     required this.libelle,
+    required this.defaultPhoto,
+    required this.defaultPhotoMobile,
     required this.prefer,
   });
 
   int id;
   String libelle;
+  String defaultPhoto;
+  String defaultPhotoMobile;
   List<dynamic> prefer;
 
   factory CategorieService.fromJson(Map<String, dynamic> json) =>
       CategorieService(
         id: json["id"],
         libelle: json["libelle"],
+        defaultPhoto: json["defaultPhoto"],
+        defaultPhotoMobile: json["defaultPhotoMobile"],
         prefer: List<dynamic>.from(json["prefer"].map((x) => x)),
       );
 
   Map<String, dynamic> toJson() => {
         "id": id,
         "libelle": libelle,
+        "defaultPhoto": defaultPhoto,
+        "defaultPhotoMobile": defaultPhotoMobile,
         "prefer": List<dynamic>.from(prefer.map((x) => x)),
       };
 }
