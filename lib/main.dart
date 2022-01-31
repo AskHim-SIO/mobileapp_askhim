68e02682d04e605f98bfc2d4e0b2d425b9bf35d0
BastiennM <bastienmetais79@gmail.com>
2022-01-31 18:10:11 +0100

    add authguard

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,56 @@
-import 'package:ap4_askhim/routes/router.gr.dart';
-import 'package:flutter/material.dart';
 import 'package:ap4_askhim/constants.dart';
-import 'package:flutter/services.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'model/token_models.dart';
+import 'package:ap4_askhim/routes/router.gr.dart';
+import 'package:ap4_askhim/services/auth_service.dart';
+import 'package:flutter/material.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:ap4_askhim/Screens/Welcome/welcome_screens.dart' as welcome;
 
-void main() async {
-  // avoid rotation screen
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  await Hive.initFlutter();
-  Hive.registerAdapter(TokenModelAdapter());
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class appBar extends StatelessWidget {
+  const appBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final _appRouter = AppRouter();
-
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: kPrimaryColor,
-          scaffoldBackgroundColor: Colors.black,
-          fontFamily: 'Inter'),
-      routerDelegate: _appRouter.delegate(),
-      routeInformationParser: _appRouter.defaultRouteParser(),
-    );
+    return AutoTabsScaffold(
+        routes: [
+          HomeRouter(),
+          SearchRouter(),
+          AddRouter(),
+          MessageRouter(),
+          ProfileRouter()
+        ],
+        bottomNavigationBuilder: (_, tabsRouter) {
+          return BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                title: Text('Acceuil'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search_outlined),
+                title: Text('Rechercher'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add_circle_outline_outlined),
+                title: Text('Ajouter'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.mail_outline),
+                title: Text('Messages'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person_outlined),
+                title: Text('Profil'),
+              ),
+            ],
+            currentIndex: tabsRouter.activeIndex,
+            unselectedItemColor: Colors.grey,
+            selectedItemColor: kPrimaryColor,
+            onTap: tabsRouter.setActiveIndex,
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            backgroundColor: Colors.white,
+          );
+        });
   }
 }
