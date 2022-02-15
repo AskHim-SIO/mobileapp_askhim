ce6f59a89e1a17c7347cf2fade3c4babf43c13c2
BastiennM <bastienmetais79@gmail.com>
2022-02-15 23:16:06 +0100

    ad back button to service page

--- lib/Screens/Home/components/body.dart
+++ lib/Screens/Home/components/body.dart
@@ -95,7 +95,6 @@ class _BodyState extends State<Body> {
                                     sizeTitle: 15,
                                     sizeSubtitle: 15),
                               );
-
                             },
                           );
                         } else {
@@ -194,8 +193,10 @@ class _BodyState extends State<Body> {
                                         borderRadius: 15,
                                         width: double.infinity,
                                         height: double.infinity,
-                                        linkImage: service!.photos[0].libelle
-                                            .toString(),
+                                        linkImage: service!.photos.isEmpty
+                                            ? service.type.defaultPhoto
+                                            : service.photos[0].libelle
+                                                .toString(),
                                         titleCard: service.name,
                                         prix: '${service.price}€',
                                         sizeTitle: 15,
--- lib/models/homeShuffleService.dart
+++ lib/models/homeShuffleService.dart
@@ -15,6 +15,9 @@ class ShuffleService {
   ShuffleService({
     required this.id,
     required this.name,
+    required this.description,
+    required this.dateStart,
+    required this.dateEnd,
     required this.price,
     required this.postDate,
     required this.lieu,
@@ -24,6 +27,9 @@ class ShuffleService {
 
   int id;
   String name;
+  String description;
+  int dateStart;
+  int dateEnd;
   int price;
   int postDate;
   Lieu lieu;
@@ -33,6 +39,9 @@ class ShuffleService {
   factory ShuffleService.fromJson(Map<String, dynamic> json) => ShuffleService(
         id: json["id"],
         name: json["name"],
+        description: json["description"],
+        dateStart: json["dateStart"],
+        dateEnd: json["dateEnd"],
         price: json["price"],
         postDate: json["postDate"],
         lieu: Lieu.fromJson(json["lieu"]),
@@ -43,6 +52,9 @@ class ShuffleService {
   Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
+        "description": description,
+        "dateStart": dateStart,
+        "dateEnd": dateEnd,
         "price": price,
         "postDate": postDate,
         "lieu": lieu.toJson(),
@@ -54,19 +66,27 @@ class ShuffleService {
 class Lieu {
   Lieu({
     required this.id,
+    required this.adresse,
+    required this.codePostal,
     required this.ville,
   });
 
   int id;
+  String adresse;
+  int codePostal;
   String ville;
 
   factory Lieu.fromJson(Map<String, dynamic> json) => Lieu(
         id: json["id"],
+        adresse: json["adresse"],
+        codePostal: json["codePostal"],
         ville: json["ville"],
       );
 
   Map<String, dynamic> toJson() => {
         "id": id,
+        "adresse": adresse,
+        "codePostal": codePostal,
         "ville": ville,
       };
 }
@@ -95,31 +115,41 @@ class Type {
   Type({
     required this.id,
     required this.libelle,
+    required this.defaultPhoto,
+    required this.defaultPhotoMobile,
     required this.prefer,
   });
 
   int id;
   Libelle? libelle;
+  String defaultPhoto;
+  String defaultPhotoMobile;
   List<dynamic> prefer;
 
   factory Type.fromJson(Map<String, dynamic> json) => Type(
         id: json["id"],
         libelle: libelleValues.map[json["libelle"]],
+        defaultPhoto: json["defaultPhoto"],
+        defaultPhotoMobile: json["defaultPhotoMobile"],
         prefer: List<dynamic>.from(json["prefer"].map((x) => x)),
       );
 
   Map<String, dynamic> toJson() => {
         "id": id,
         "libelle": libelleValues.reverse[libelle],
+        "defaultPhoto": defaultPhoto,
+        "defaultPhotoMobile": defaultPhotoMobile,
         "prefer": List<dynamic>.from(prefer.map((x) => x)),
       };
 }
 
-enum Libelle { LOISIR, TRANSPORT, COURSE }
+enum Libelle { TRANSPORT, TCHE_MNAGRE, LOISIR, FORMATION, COURSE }
 
 final libelleValues = EnumValues({
   "Course": Libelle.COURSE,
+  "Formation": Libelle.FORMATION,
   "Loisir": Libelle.LOISIR,
+  "Tâche ménagère": Libelle.TCHE_MNAGRE,
   "Transport": Libelle.TRANSPORT
 });
 
